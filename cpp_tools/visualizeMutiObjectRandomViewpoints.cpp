a99daa508598e7c624b9a1cdb20f0799ef4351ad
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-17 22:44:52 -0400

    Added collision check while doing random sampling

--- cpp_tools/visualizeMutiObjectRandomViewpoints.cpp
+++ cpp_tools/visualizeMutiObjectRandomViewpoints.cpp
@@ -12,6 +12,7 @@
 #include <CuteGL/Core/PoseUtils.h>
 #include <CuteGL/Core/MeshUtils.h>
 #include <CuteGL/Core/ColorUtils.h>
+#include <CuteGL/Geometry/OrientedBoxHelper.h>
 
 #include <Eigen/EulerAngles>
 
@@ -20,29 +21,24 @@
 #include <fstream>
 #include <iostream>
 
-std::vector<Eigen::Vector4f, Eigen::aligned_allocator<Eigen::Vector4f> > load_viewpoint_distribution(
+std::vector<Eigen::Vector3f> load_viewpoint_distribution(
     const std::string& vp_distrib_file) {
   std::ifstream file(vp_distrib_file.c_str(), std::ios::in);
   if (!file.is_open()) {
     throw std::runtime_error("Cannot open File from " + vp_distrib_file);
   }
 
-  std::vector<Eigen::Vector4f, Eigen::aligned_allocator<Eigen::Vector4f> > viewpoints;
-
-  std::mt19937 gen(std::random_device { }());  //Standard mersenne_twister_engine seeded with rd()
-  std::uniform_real_distribution<float> dis(1.0f, 30.0f);
+  std::vector<Eigen::Vector3f> viewpoints;
 
   std::string line;
   while (std::getline(file, line)) {
     std::istringstream iss(line);
-    Eigen::Vector4f vp;
-    if (!(iss >> vp[0] >> vp[1] >> vp[2] >> vp[3])) {
+    Eigen::Vector3f vp;
+    if (!(iss >> vp[0] >> vp[1] >> vp[2])) {
       break;
     }  // error
 
-    vp[3] = dis(gen);
-
-    vp.head<3>() *= M_PI / 180.0f;
+    vp *= M_PI / 180.0f;
 
     viewpoints.push_back(vp);
   }
@@ -100,20 +96,41 @@ class ViewpointBrowser : public WindowRenderViewer {
 
         std::uniform_real_distribution<float> x_dis(0.0f, width());
         std::uniform_real_distribution<float> y_dis(0.0f, height());
+        std::uniform_real_distribution<float> z_dis(1.0f, 30.0f);
 
+        MultiObjectRenderer::ModelDrawers::Poses& poses = renderer_->modelDrawers().poses();
         const Eigen::Matrix3f Kinv = K_.inverse();
+        const std::size_t number_of_objects = renderer_->modelDrawers().poses().size();
 
-        for (auto& pose : renderer_->modelDrawers().poses()) {
+        for (size_t i = 0; i < number_of_objects; ++i) {
           ++vp_index_;
           if (vp_index_ >= vp_indices_.size()) {
             vp_index_ = 0;
             std::shuffle(vp_indices_.begin(), vp_indices_.end(), rnd_eng_);
           }
-          Eigen::Isometry3f vp_pose = getExtrinsicsFromViewPoint(viewpoints_[vp_indices_[vp_index_]]);
+          const Eigen::Vector3f& vp = viewpoints_[vp_indices_[vp_index_]];
 
-          Eigen::Vector3f center_proj_ray = Kinv * Eigen::Vector3f(x_dis(rnd_eng_), y_dis(rnd_eng_), 1.0f);
+          // Loop and keep sampling new center_proj until we find a collision free pose
+          while (true) {
+            Eigen::Isometry3f vp_pose = getExtrinsicsFromViewPoint(vp.x(), vp.y(), vp.z(), z_dis(rnd_eng_));
+            Eigen::Vector3f center_proj_ray = Kinv * Eigen::Vector3f(x_dis(rnd_eng_), y_dis(rnd_eng_), 1.0f);
+            Eigen::Isometry3f pose = Eigen::Quaternionf::FromTwoVectors(Eigen::Vector3f::UnitZ(), center_proj_ray) * vp_pose;
 
-          pose = Eigen::Quaternionf::FromTwoVectors(Eigen::Vector3f::UnitZ(), center_proj_ray) * vp_pose;
+            bool collision_free = true;
+
+            // check for all previous poses for collision
+            for (size_t j = 0; j < i; ++j) {
+              if (checkOrientedBoxCollision(pose, Eigen::Vector3f::Ones(), poses[j], Eigen::Vector3f::Ones())) {
+                collision_free = false;
+                break;
+              }
+            }
+
+            if (collision_free) {
+              poses[i] = pose;
+              break;
+            }
+          }
         }
         break;
       }
@@ -123,7 +140,7 @@ class ViewpointBrowser : public WindowRenderViewer {
     }
   }
   MultiObjectRenderer* renderer_;
-  std::vector<Eigen::Vector4f, Eigen::aligned_allocator<Eigen::Vector4f> > viewpoints_;
+  std::vector<Eigen::Vector3f> viewpoints_;
   std::vector<std::size_t> vp_indices_;
   std::size_t vp_index_;
   std::mt19937 rnd_eng_;
@@ -155,7 +172,7 @@ int main(int argc, char **argv) {
   const float focal_length = 1750.0f;
 
   viewer.resize(W, H);
-  viewer.setSceneRadius(3.0f);
+  viewer.setSceneRadius(50.0f);
 
   viewer.camera().extrinsics() = Isometry3f::Identity();
   viewer.showAndWaitTillExposed();
