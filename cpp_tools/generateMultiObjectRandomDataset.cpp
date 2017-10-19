fc4603be925cf63ed09b60bfae88c6a7dca397b3
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-18 21:16:35 -0400

    Adding amodal bbx generation

--- cpp_tools/generateMultiObjectRandomDataset.cpp
+++ cpp_tools/generateMultiObjectRandomDataset.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "RenderFor3Data/Config.h"
+#include "RenderFor3Data/ImageDataset.h"
 #include <CuteGL/Renderer/MultiObjectRenderer.h>
 #include <CuteGL/Surface/OffScreenRenderViewer.h>
 #include <CuteGL/Core/Config.h>
@@ -29,6 +30,39 @@
 #include <fstream>
 #include <iostream>
 
+namespace fs = boost::filesystem;
+
+// TODO: In newer boost or c++17 we have relative
+fs::path relative_path(fs::path to, fs::path from) {
+   // Start at the root path and while they are the same then do nothing then when they first
+   // diverge take the remainder of the two path and replace the entire from path with ".."
+   // segments.
+   fs::path::const_iterator fromIter = from.begin();
+   fs::path::const_iterator toIter = to.begin();
+
+   // Loop through both
+   while (fromIter != from.end() && toIter != to.end() && (*toIter) == (*fromIter))
+   {
+      ++toIter;
+      ++fromIter;
+   }
+
+   fs::path finalPath;
+   while (fromIter != from.end())
+   {
+      finalPath /= "..";
+      ++fromIter;
+   }
+
+   while (toIter != to.end())
+   {
+      finalPath /= *toIter;
+      ++toIter;
+   }
+
+   return finalPath;
+}
+
 class MultiObjectDatasetGenerator {
  public:
   MultiObjectDatasetGenerator(const std::size_t num_of_objects_per_image)
@@ -38,7 +72,7 @@ class MultiObjectDatasetGenerator {
         vp_index_(0),
         model_index_(0),
         rnd_eng_ { std::random_device { }() },
-        K_(Eigen::Matrix3f::Identity()) {
+        K_(Eigen::Matrix3d::Identity()) {
 
     viewer_.setBackgroundColor(0, 0, 0);
     renderer_->setDisplayAxis(false);
@@ -49,12 +83,12 @@ class MultiObjectDatasetGenerator {
 
     const int W = 1600;
     const int H = 800;
-    const float focal_length = 1760.0f;
+    const double focal_length = 1760.0f;
 
-    K_ << focal_length, 0.0f, W / 2.0f, 0.0f, focal_length, H / 2.0f, 0.0f, 0.0f, 1.0f;
+    K_ << focal_length, 0.0, W / 2.0, 0.0, focal_length, H / 2.0, 0.0f, 0.0f, 1.0;
 
     viewer_.resize(W, H);
-    viewer_.camera().intrinsics() = CuteGL::getGLPerspectiveProjection(K_, W, H, 0.1f, 50.0f);
+    viewer_.camera().intrinsics() = CuteGL::getGLPerspectiveProjection(K_.cast<float>(), W, H, 0.1f, 50.0f);
     viewer_.camera().extrinsics() = Eigen::Isometry3f::Identity();
 
     viewer_.create();
@@ -63,6 +97,13 @@ class MultiObjectDatasetGenerator {
     renderer_->phongShader().program.bind();
     renderer_->phongShader().setLightPosition(0.0f, -50.0f, 10.0f);
     renderer_->phongShader().program.release();
+
+    dataset_.name = "RandomFlyingCars";
+    dataset_.rootdir = fs::path(RENDERFOR3DATA_ROOT_DIR) / fs::path("data");
+    assert(fs::exists(dataset_.rootdir));
+
+    image_file_fmt_ = boost::format((dataset_.rootdir / "RandomFlyingCars" / "color_gl"/ "%08i_color.png").string());
+    segm_file_fmt_ = boost::format((dataset_.rootdir / "RandomFlyingCars" / "segm_gl"/ "%08i_segm.png").string());
   }
 
   CuteGL::OffScreenRenderViewer& viewer() {return viewer_;}
@@ -77,12 +118,15 @@ class MultiObjectDatasetGenerator {
     std::string line;
     while (std::getline(file, line)) {
       std::istringstream iss(line);
-      Eigen::Vector3f vp;
+      Eigen::Vector3d vp;
       if (!(iss >> vp[0] >> vp[1] >> vp[2])) {
         break;
       }  // error
 
       vp *= M_PI / 180.0f;
+      vp[0] = CuteGL::wrapToPi(vp[0]);
+      vp[1] = CuteGL::wrapToPi(vp[1]);
+      vp[2] = CuteGL::wrapToPi(vp[2]);
 
       viewpoints_.push_back(vp);
     }
@@ -121,7 +165,7 @@ class MultiObjectDatasetGenerator {
     assert(model_filepaths_.size() == model_indices_.size());
     assert(num_of_objects_per_image_ <= model_indices_.size());
     renderer_->modelDrawers().clear();
-    model_bbx_sizes_.clear();
+    cb_obj_infos_.clear();
 
     std::uniform_real_distribution<float> hue_dist(0.0f, 1.0f);
     std::uniform_real_distribution<float> sat_dist(0.95f, 1.0f);
@@ -134,10 +178,16 @@ class MultiObjectDatasetGenerator {
         model_index_ = 0;
         std::shuffle(model_indices_.begin(), model_indices_.end(), rnd_eng_);
       }
+      const std::string model_file = model_filepaths_.at(model_indices_.at(model_index_));
 
-      CuteGL::MeshData legacy_mesh = CuteGL::loadMeshFromPLY(model_filepaths_.at(model_indices_.at(model_index_)));
+      ImageObjectInfo obj_info;
+      obj_info.id = i + 1;
+      obj_info.category = "car";
+      obj_info.shape_file = relative_path(model_file, dataset_.rootdir).string();
+
+      CuteGL::MeshData legacy_mesh = CuteGL::loadMeshFromPLY(model_file);
       Eigen::AlignedBox3f bbx  = computeAlignedBox(legacy_mesh);
-      model_bbx_sizes_.push_back(bbx.sizes());
+      obj_info.dimension = bbx.sizes().cast<double>();
 
       const float golden_ratio_conjugate = 0.618033988749895f;
       const float hue = 360.0f * std::fmod(hue_dist(rnd_eng_) + golden_ratio_conjugate, 1.0f);
@@ -157,13 +207,15 @@ class MultiObjectDatasetGenerator {
         mesh.colors.resize(legacy_mesh.vertices.size(), Eigen::NoChange);
         mesh.colors.rowwise() = color.transpose();
 
-        mesh.labels.setConstant(legacy_mesh.vertices.size(), i+1);
+        mesh.labels.setConstant(legacy_mesh.vertices.size(), obj_info.id.value());
 
         mesh.faces = legacy_mesh.faces;
       }
 
 
       renderer_->modelDrawers().addItem(Eigen::Affine3f::Identity(), mesh);
+
+      cb_obj_infos_.push_back(obj_info);
     }
 
     assert(model_filepaths_.size() == model_indices_.size());
@@ -171,45 +223,112 @@ class MultiObjectDatasetGenerator {
 
   void generateModelPoses() {
     assert(viewpoints_.size() == vp_indices_.size());
-    std::uniform_real_distribution<float> x_dis(0.0f, viewer_.width());
-    std::uniform_real_distribution<float> y_dis(0.0f, viewer_.height());
-    std::uniform_real_distribution<float> z_dis(1.0f, 30.0f);
+    std::uniform_real_distribution<double> x_dis(0.0, viewer_.width());
+    std::uniform_real_distribution<double> y_dis(0.0, viewer_.height());
+    std::uniform_real_distribution<double> z_dis(1.0, 30.0);
 
     auto& poses = renderer_->modelDrawers().poses();
-    const Eigen::Matrix3f Kinv = K_.inverse();
+    const Eigen::Matrix3d Kinv = K_.inverse();
     const std::size_t number_of_objects = renderer_->modelDrawers().poses().size();
-    assert(model_bbx_sizes_.size() == number_of_objects);
+    assert(cb_obj_infos_.size() == number_of_objects);
 
     for (size_t i = 0; i < number_of_objects; ++i) {
+      ImageObjectInfo& obj_info_i = cb_obj_infos_.at(i);
+
       ++vp_index_;
       if (vp_index_ >= vp_indices_.size()) {
         vp_index_ = 0;
         std::shuffle(vp_indices_.begin(), vp_indices_.end(), rnd_eng_);
       }
-      const Eigen::Vector3f& vp = viewpoints_[vp_indices_[vp_index_]];
+      const Eigen::Vector3d& vp = viewpoints_[vp_indices_[vp_index_]];
+      obj_info_i.viewpoint = vp;
 
       // Loop and keep sampling new center_proj until we find a collision free pose
       while (true) {
-        Eigen::Isometry3f vp_pose = CuteGL::getExtrinsicsFromViewPoint(vp.x(), vp.y(), vp.z(), z_dis(rnd_eng_));
-        Eigen::Vector3f center_proj_ray = Kinv * Eigen::Vector3f(x_dis(rnd_eng_), y_dis(rnd_eng_), 1.0f);
-        Eigen::Isometry3f pose = Eigen::Quaternionf::FromTwoVectors(Eigen::Vector3f::UnitZ(), center_proj_ray) * vp_pose;
+        double center_dist = z_dis(rnd_eng_);
+        Eigen::Isometry3d vp_pose = CuteGL::getExtrinsicsFromViewPoint(vp.x(), vp.y(), vp.z(), center_dist);
+        Eigen::Vector2d center_proj(x_dis(rnd_eng_), y_dis(rnd_eng_));
+        Eigen::Vector3d center_proj_ray = Kinv * center_proj.homogeneous();
+        Eigen::Isometry3d pose = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitZ(), center_proj_ray) * vp_pose;
 
         bool collision_free = true;
 
         // check for all previous poses for collision
         for (size_t j = 0; j < i; ++j) {
-          if (CuteGL::checkOrientedBoxCollision(pose, model_bbx_sizes_.at(i), poses.at(j), model_bbx_sizes_.at(j))) {
+          ImageObjectInfo& obj_info_j = cb_obj_infos_.at(j);
+          if (CuteGL::checkOrientedBoxCollision(pose, obj_info_i.dimension.value(), poses.at(j).cast<double>(), obj_info_j.dimension.value())) {
             collision_free = false;
             break;
           }
         }
 
         if (collision_free) {
-          poses[i] = pose;
+          poses[i] = pose.cast<float>();
+          obj_info_i.center_proj = center_proj;
+          obj_info_i.center_dist = center_dist;
           break;
         }
       }
     }
+  }
+
+  void render_and_check() {
+    const std::size_t number_of_objects = renderer_->modelDrawers().poses().size();
+    assert(cb_obj_infos_.size() == number_of_objects);
+
+    const int H = viewer_.height();
+    const int W = viewer_.width();
+
+    ImageInfo img_info;
+    img_info.image_intrinsic = K_;
+    img_info.image_size = Eigen::Vector2i(W, H);
+
+    const Eigen::Matrix3d K_inv = K_.inverse();
+
+    // Render and update bbx_visible
+    viewer_.render();
+    {
+      // Save color image
+      QImage color_image = viewer_.readColorBuffer();
+      std::string image_file_path = boost::str(boost::format(image_file_fmt_) % dataset_.image_infos.size());
+      color_image.save(QString::fromStdString(image_file_path));
+      img_info.image_file = relative_path(image_file_path, dataset_.rootdir).string();
+    }
+
+    using Image32FC1 = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+    using Image8UC1 = Eigen::Matrix<unsigned char, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+
+    Image8UC1 label_image(H, W);
+    {
+
+      Image32FC1 image_32FC1(H, W);
+      viewer_.readLabelBuffer(image_32FC1.data());
+      image_32FC1.colwise().reverseInPlace();
+      label_image = image_32FC1.cast<unsigned char>();
+      cv::Mat cv_image(H, W, CV_8UC1, label_image.data());
+      std::string segm_file_path = boost::str(boost::format(segm_file_fmt_) % dataset_.image_infos.size());
+      cv::imwrite(segm_file_path, cv_image);
+      img_info.segm_file = relative_path(segm_file_path, dataset_.rootdir).string();
+    }
+
+    for (size_t i = 0; i < number_of_objects; ++i) {
+      ImageObjectInfo& obj_info = cb_obj_infos_.at(i);
+      const Eigen::Vector3d& vp = obj_info.viewpoint.value();
+      Eigen::Isometry3d vp_pose = CuteGL::getExtrinsicsFromViewPoint(vp.x(), vp.y(), vp.z(), obj_info.center_dist.value());
+      Eigen::Vector3d center_proj_ray = K_inv * obj_info.center_proj.value().homogeneous();
+      Eigen::Isometry3d pose = Eigen::Quaterniond::FromTwoVectors(Eigen::Vector3d::UnitZ(), center_proj_ray) * vp_pose;
+
+      Eigen::Matrix<double, 2, 8> img_corners = (img_info.image_intrinsic.value() * CuteGL::computeOrientedBoxCorners(pose, obj_info.dimension.value())).colwise().hnormalized();
+      obj_info.bbx_amodal = Eigen::Vector4d(img_corners.row(0).minCoeff(), img_corners.row(1).minCoeff(),
+                                            img_corners.row(0).maxCoeff(), img_corners.row(1).maxCoeff());
+    }
+
+    img_info.object_infos = cb_obj_infos_;
+    dataset_.image_infos.push_back(img_info);
+  }
+
+  void save_dataset(const std::string& filename) {
+    saveImageDatasetToJson(dataset_, filename);
   }
 
  private:
@@ -218,7 +337,7 @@ class MultiObjectDatasetGenerator {
 
   std::size_t num_of_objects_per_image_;
 
-  std::vector<Eigen::Vector3f> viewpoints_;
+  std::vector<Eigen::Vector3d> viewpoints_;
   std::vector<std::size_t> vp_indices_;
   std::size_t vp_index_;
 
@@ -226,9 +345,14 @@ class MultiObjectDatasetGenerator {
   std::vector<std::size_t> model_indices_;
   std::size_t model_index_;
 
-  std::vector<Eigen::Vector3f> model_bbx_sizes_;
   std::mt19937 rnd_eng_;
-  Eigen::Matrix3f K_;
+  Eigen::Matrix3d K_;
+
+  ImageInfo::ImageObjectInfos cb_obj_infos_;
+  ImageDataset dataset_;
+
+  boost::format image_file_fmt_;
+  boost::format segm_file_fmt_;
 };
 
 int main(int argc, char **argv) {
@@ -251,32 +375,18 @@ int main(int argc, char **argv) {
 
   std::cout << "Rendering Images ..." << std::endl;
 
-  const int H = dataset_generator.viewer().height();
-  const int W = dataset_generator.viewer().width();
-
-  using Image32FC1 = Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-  using Image16SC1 = Eigen::Matrix<short int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
-
-  Image32FC1 image_32FC1(H, W);
-  Image16SC1 image_16SC1(H, W);
-
-  const boost::format image_out_file_fmt("test_%i.png");
-
   const int num_of_images_to_generate = 100;
   boost::progress_display show_progress(num_of_images_to_generate);
   for (int i = 0; i<num_of_images_to_generate; ++i ) {
     dataset_generator.loadNextBatchOfModels();
     dataset_generator.generateModelPoses();
-    dataset_generator.viewer().render();
-
-    dataset_generator.viewer().readLabelBuffer(image_32FC1.data());
-    image_32FC1.colwise().reverseInPlace();
-    image_16SC1 = image_32FC1.cast<short int>();
-
-    cv::Mat cv_image(H, W, CV_16SC1, image_16SC1.data());
-    cv::imwrite(boost::str(boost::format(image_out_file_fmt) % i), cv_image);
+    dataset_generator.render_and_check();
     ++show_progress;
   }
+
+  std::cout << "Saving dataset ..." << std::flush;
+  dataset_generator.save_dataset("temp.json");
+  std::cout << "Done." << std::endl;
 
   return EXIT_SUCCESS;
 }
