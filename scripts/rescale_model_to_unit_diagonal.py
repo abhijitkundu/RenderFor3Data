dddab7f78c10050fd6da7256e6ff1a22eb36e5a7
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-26 17:18:54 -0400

    recenters and rescales a model to have unit diagonal and centered at world origin

---
+++ scripts/rescale_model_to_unit_diagonal.py
@@ -0,0 +1,45 @@
+import argparse
+import sys
+import os.path as osp
+
+import bpy
+from mathutils import Matrix, Vector
+
+# from RenderFor3Data.blender_helper import print_blender_object_atrributes
+
+def main():
+    """main function"""
+    # parse commandline arguments
+    argv = sys.argv
+    argv = argv[argv.index("--") + 1:]  # get all args after "--"
+
+    parser = argparse.ArgumentParser(description='Transform obj model')
+    parser.add_argument('obj_file', type=str, help='path to obj_file file')
+    args = parser.parse_args(argv)
+
+    # clear default lights
+    bpy.ops.object.select_by_type(type='LAMP')
+    bpy.ops.object.delete(use_global=False)
+
+    # clear default Cube object
+    bpy.data.objects['Cube'].select = True
+    bpy.ops.object.delete(use_global=False)
+
+    # import obj file
+    bpy.ops.import_scene.obj(filepath=args.obj_file, split_mode='OFF')
+    model = bpy.context.selected_objects[0]
+
+    # print_blender_object_atrributes(model)
+
+    bbx_center = 0.125 * sum((Vector(b) for b in model.bound_box), Vector())
+    diag_length = model.dimensions.length
+
+    if bbx_center.length > 1e-6 or abs(diag_length - 1.0) > 1e-6:
+        print("Updating model file at {}".format(args.obj_file))
+        model.location = - (model.matrix_world * bbx_center)
+        bpy.ops.export_scene.obj(filepath=args.obj_file, use_selection=True, global_scale=1.0/diag_length)
+    else:
+        print("Skipping model file at {}".format(args.obj_file))
+
+if __name__ == '__main__':
+    main()
