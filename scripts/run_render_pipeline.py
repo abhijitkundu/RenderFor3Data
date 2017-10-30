8c28d00afb0dea95b2379066e07295c9124778be
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-10-30 00:37:17 -0400

    Added flexibility of choosing gpus

--- scripts/run_render_pipeline.py
+++ scripts/run_render_pipeline.py
@@ -18,7 +18,7 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("image_infos_dir", nargs=1, type=str, help="Path to output directory")
-    parser.add_argument("-g", "--num_of_gpus", default=1, type=int, help="Number of gpus in system.")
+    parser.add_argument("-g", "--gpus", nargs='+', default=[0], type=int, help="GPUs to use.")
     parser.add_argument("-t", "--num_of_threads", default=12, type=int, help="Number of parallel threads to use")
     parser.add_argument("-s", "--render_script", default=default_render_script, type=str, help="Path to render script")
     parser.add_argument("-e", "--extra_args", default='', type=str, help="extra arguements for the rendering script")
@@ -29,20 +29,22 @@ def main():
     image_infos_dir = args.image_infos_dir[0]
     assert osp.isdir(image_infos_dir), "{} is not a directory".format(image_infos_dir)
     assert osp.exists(image_infos_dir), "Directory {} do not exist".format(image_infos_dir)
-
-    assert args.num_of_threads > 0
-    assert args.num_of_gpus > 0
-
     assert osp.exists(args.render_script), "render script {} do not exist".format(args.render_script)
+
+    assert args.num_of_threads > 0, "Number of threads need to be +ve"
+    num_of_gpus = len(args.gpus)
+    assert num_of_gpus > 0, "You need to provide atleast one gpu"
 
     # Glob image info files
     image_info_files = glob(osp.join(image_infos_dir, '*_info.json'))
     assert image_info_files, "No image_info json files found in {}".format(image_infos_dir)
 
+    print("Rendering will use {} gpus = {}".format(num_of_gpus, args.gpus))
+
     print('Generating rendering commands ...')
     commands = []
     for idx, image_info_file in enumerate(tqdm(image_info_files)):
-        gpu_id = idx % args.num_of_gpus
+        gpu_id = args.gpus[idx % num_of_gpus]
         command = "blender --background --python {} -- {} -g {} {} > /dev/null 2>&1".format(args.render_script, image_info_file, gpu_id, args.extra_args)
         commands.append(command)
 
@@ -51,7 +53,7 @@ def main():
             print(cmd)
     else:
         pool = Pool(args.num_of_threads)
-        print ('Rendering images with {} threads and {} gpus. This can take days.'.format(args.num_of_threads, args.num_of_gpus))
+        print ('Rendering images with {} threads and {} gpus (). This can take days.'.format(args.num_of_threads, num_of_gpus, args.gpus))
         for idx, return_code in enumerate(tqdm(pool.imap(partial(call, shell=True), commands))):
             if return_code != 0:
                 print('Rendering failed for {}'.format(image_info_files[idx]))
