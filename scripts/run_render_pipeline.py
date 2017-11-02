b2329d67bcccde9bb6ea59e34780d21b184aa449
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-11-02 01:20:42 -0400

    run_render_pipeline now directly takes the image info files as input rather than directory

--- scripts/run_render_pipeline.py
+++ scripts/run_render_pipeline.py
@@ -3,7 +3,6 @@
 import argparse
 import os.path as osp
 from functools import partial
-from glob import glob
 from multiprocessing.dummy import Pool
 from subprocess import call
 
@@ -17,7 +16,7 @@ def main():
     default_render_script = osp.join(root_dir, 'render_single_image_info.py')
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("image_infos_dir", nargs=1, type=str, help="Path to image_infos files directory")
+    parser.add_argument("-i", "--image_infos_files", nargs='+', type=str, help="Path to image_infos files")
     parser.add_argument("-g", "--gpus", nargs='+', default=[0], type=int, help="GPUs to use.")
     parser.add_argument("-t", "--num_of_threads", default=12, type=int, help="Number of parallel threads to use")
     parser.add_argument("-s", "--render_script", default=default_render_script, type=str, help="Path to render script")
@@ -26,24 +25,21 @@ def main():
     parser.set_defaults(dryrun=False)
     args = parser.parse_args()
 
-    image_infos_dir = args.image_infos_dir[0]
-    assert osp.isdir(image_infos_dir), "{} is not a directory".format(image_infos_dir)
-    assert osp.exists(image_infos_dir), "Directory {} do not exist".format(image_infos_dir)
     assert osp.exists(args.render_script), "render script {} do not exist".format(args.render_script)
 
     assert args.num_of_threads > 0, "Number of threads need to be +ve"
     num_of_gpus = len(args.gpus)
     assert num_of_gpus > 0, "You need to provide atleast one gpu"
 
-    # Glob image info files
-    image_info_files = glob(osp.join(image_infos_dir, '*_info.json'))
-    assert image_info_files, "No image_info json files found in {}".format(image_infos_dir)
-
     print("Rendering will use {} gpus = {}".format(num_of_gpus, args.gpus))
 
-    print('Generating rendering commands ...')
+    print('Generating rendering commands for {} images ...'.format(len(args.image_infos_files)))
     commands = []
-    for idx, image_info_file in enumerate(tqdm(image_info_files)):
+    for idx, image_info_file in enumerate(tqdm(args.image_infos_files)):
+        assert osp.exists(image_info_file), "{} do not exist".format(image_info_file)
+        assert osp.isfile(image_info_file), "{} is not a file".format(image_info_file)
+        assert osp.splitext(image_info_file)[1] == '.json', "File {} is not avalid json file".format(image_info_file)
+
         gpu_id = args.gpus[idx % num_of_gpus]
         command = "blender --background --python {} -- {} -g {} {} > /dev/null 2>&1".format(args.render_script, image_info_file, gpu_id, args.extra_args)
         commands.append(command)
@@ -56,7 +52,7 @@ def main():
         print ('Rendering images with {} threads and {} gpus ({}). This can take days.'.format(args.num_of_threads, num_of_gpus, args.gpus))
         for idx, return_code in enumerate(tqdm(pool.imap(partial(call, shell=True), commands))):
             if return_code != 0:
-                print('Rendering failed for {}'.format(image_info_files[idx]))
+                print('Rendering failed for {}'.format(args.image_info_files[idx]))
 
 
 if __name__ == '__main__':
