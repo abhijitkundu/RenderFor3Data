b1a958f4de1eab2ccfb95b04b87d367d167a6978
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-11-07 18:49:52 -0500

    more cleanup

--- render_scripts/render_smpl.py
+++ render_scripts/render_smpl.py
@@ -14,16 +14,6 @@ import bpy
 from mathutils import Matrix, Vector, Quaternion, Euler
 from bpy_extras.object_utils import world_to_camera_view as world2cam
 
-# sys.path.insert(0, ".")
-
-
-def mkdir_safe(directory):
-    try:
-        os.makedirs(directory)
-    except FileExistsError:
-        pass
-
-
 def setState0():
     for ob in bpy.data.objects.values():
         ob.select = False
@@ -44,14 +34,14 @@ part_match = {'root': 'root', 'bone_00': 'Pelvis', 'bone_01': 'L_Hip', 'bone_02'
 
 part2num = {part: (ipart + 1) for ipart, part in enumerate(sorted_parts)}
 
-# create one material per part as defined in a pickle with the segmentation
-# this is useful to render the segmentation in a material pass
-
-
-def create_segmentation(ob):
+def create_segmentation(ob, segm_overlap_file):
+    """
+    create one material per part as defined in a pickle with the segmentation
+    this is useful to render the segmentation in a material pass
+    """
     materials = {}
     vgroups = {}
-    with open('/home/abhijit/Scratchspace/SURREAL/smpl_data/segm_per_v_overlap.pkl', 'rb') as f:
+    with open(segm_overlap_file, 'rb') as f:
         vsegm = load(f)
     bpy.ops.object.material_slot_remove()
     parts = sorted(vsegm.keys())
@@ -110,20 +100,7 @@ def create_sh_material(tree, sh_path, img=None):
     tree.links.new(script.outputs[0], emission.inputs[0])
     tree.links.new(emission.outputs[0], mat_out.inputs[0])
 
-# computes rotation matrix through Rodrigues formula as in cv2.Rodrigues
-
-
-def Rodrigues(rotvec):
-    theta = np.linalg.norm(rotvec)
-    r = (rotvec / theta).reshape(3, 1) if theta > 0. else rotvec
-    cost = np.cos(theta)
-    mat = np.asarray([[0, -r[2], r[1]],
-                      [r[2], 0, -r[0]],
-                      [-r[1], r[0], 0]])
-    return(cost * np.eye(3) + (1 - cost) * r.dot(r.T) + np.sin(theta) * mat)
-
-
-def init_scene(scene, camera_params, fbx_file):
+def init_scene(scene, fbx_file):
     # load fbx model
     bpy.ops.import_scene.fbx(filepath=fbx_file, axis_forward='Y', axis_up='Z', global_scale=100)
 
@@ -141,20 +118,7 @@ def init_scene(scene, camera_params, fbx_file):
     bpy.data.objects['Cube'].select = True
     bpy.ops.object.delete(use_global=False)
 
-    # set camera properties and initial position
-    bpy.ops.object.select_all(action='DESELECT')
-    cam_ob = bpy.data.objects['Camera']
     scn = bpy.context.scene
-    scn.objects.active = cam_ob
-
-    cam_ob.matrix_world = Matrix(((0., 0., 1, camera_params['camera_distance']),
-                                  (0., -1, 0., -1.0),
-                                  (-1., 0., 0., 0.),
-                                  (0.0, 0.0, 0.0, 1.0)))
-    cam_ob.data.angle = math.radians(40)
-    cam_ob.data.lens = 60
-    cam_ob.data.clip_start = 0.1
-    cam_ob.data.sensor_width = 32
 
     # setup an empty object in the center which will be the parent of the Camera
     # this allows to easily rotate an object around the origin
@@ -165,115 +129,12 @@ def init_scene(scene, camera_params, fbx_file):
     scene.render.layers['RenderLayer'].use_pass_emit = True
     scene.render.layers['RenderLayer'].use_pass_material_index = True
 
-    # set render size
-    scn.render.resolution_x = camera_params['resy']
-    scn.render.resolution_y = camera_params['resx']
-    scn.render.resolution_percentage = 100
-    scn.render.image_settings.file_format = 'PNG'
-
     # clear existing animation data
     ob.data.shape_keys.animation_data_clear()
     arm_ob = bpy.data.objects['Armature']
     arm_ob.animation_data_clear()
 
-    return(ob, obname, arm_ob, cam_ob)
-
-# transformation between pose and blendshapes
-
-
-def rodrigues2bshapes(pose):
-    rod_rots = np.asarray(pose).reshape(24, 3)
-    mat_rots = [Rodrigues(rod_rot) for rod_rot in rod_rots]
-    bshapes = np.concatenate([(mat_rot - np.eye(3)).ravel()
-                              for mat_rot in mat_rots[1:]])
-    return(mat_rots, bshapes)
-
-
-# apply trans pose and shape to character
-def apply_trans_pose_shape(trans, pose, shape, ob, arm_ob, obname, scene, cam_ob, frame=None):
-    # transform pose into rotation matrices (for pose) and pose blendshapes
-    mrots, bsh = rodrigues2bshapes(pose)
-
-    # set the location of the first bone to the translation parameter
-    arm_ob.pose.bones[obname + '_Pelvis'].location = trans
-    if frame is not None:
-        arm_ob.pose.bones[obname + '_root'].keyframe_insert('location', frame=frame)
-    # set the pose of each bone to the quaternion specified by pose
-    for ibone, mrot in enumerate(mrots):
-        bone = arm_ob.pose.bones[obname + '_' + part_match['bone_%02d' % ibone]]
-        bone.rotation_quaternion = Matrix(mrot).to_quaternion()
-        if frame is not None:
-            bone.keyframe_insert('rotation_quaternion', frame=frame)
-            bone.keyframe_insert('location', frame=frame)
-
-    # apply pose blendshapes
-    for ibshape, bshape in enumerate(bsh):
-        ob.data.shape_keys.key_blocks['Pose%03d' % ibshape].value = bshape
-        if frame is not None:
-            ob.data.shape_keys.key_blocks['Pose%03d' % ibshape].keyframe_insert('value', index=-1, frame=frame)
-
-    # apply shape blendshapes
-    for ibshape, shape_elem in enumerate(shape):
-        ob.data.shape_keys.key_blocks['Shape%03d' % ibshape].value = shape_elem
-        if frame is not None:
-            ob.data.shape_keys.key_blocks['Shape%03d' % ibshape].keyframe_insert('value', index=-1, frame=frame)
-
-
-def get_bone_locs(obname, arm_ob, scene, cam_ob):
-    n_bones = 24
-    render_scale = scene.render.resolution_percentage / 100
-    render_size = (int(scene.render.resolution_x * render_scale),
-                   int(scene.render.resolution_y * render_scale))
-    bone_locations_2d = np.empty((n_bones, 2))
-    bone_locations_3d = np.empty((n_bones, 3), dtype='float32')
-
-    # obtain the coordinates of each bone head in image space
-    for ibone in range(n_bones):
-        bone = arm_ob.pose.bones[obname + '_' + part_match['bone_%02d' % ibone]]
-        co_2d = world2cam(scene, cam_ob, arm_ob.matrix_world * bone.head)
-        co_3d = arm_ob.matrix_world * bone.head
-        bone_locations_3d[ibone] = (co_3d.x,
-                                    co_3d.y,
-                                    co_3d.z)
-        bone_locations_2d[ibone] = (round(co_2d.x * render_size[0]),
-                                    round(co_2d.y * render_size[1]))
-    return(bone_locations_2d, bone_locations_3d)
-
-
-# reset the joint positions of the character according to its new shape
-def reset_joint_positions(orig_trans, shape, ob, arm_ob, obname, scene, cam_ob, reg_ivs, joint_reg):
-    # since the regression is sparse, only the relevant vertex
-    #     elements (joint_reg) and their indices (reg_ivs) are loaded
-    reg_vs = np.empty((len(reg_ivs), 3))  # empty array to hold vertices to regress from
-    # zero the pose and trans to obtain joint positions in zero pose
-    apply_trans_pose_shape(orig_trans, np.zeros(72), shape, ob, arm_ob, obname, scene, cam_ob)
-
-    # obtain a mesh after applying modifiers
-    bpy.ops.wm.memory_statistics()
-    # me holds the vertices after applying the shape blendshapes
-    me = ob.to_mesh(scene, True, 'PREVIEW')
-
-    # fill the regressor vertices matrix
-    for iiv, iv in enumerate(reg_ivs):
-        reg_vs[iiv] = me.vertices[iv].co
-    bpy.data.meshes.remove(me)
-
-    # regress joint positions in rest pose
-    joint_xyz = joint_reg.dot(reg_vs)
-    # adapt joint positions in rest pose
-    arm_ob.hide = False
-    bpy.ops.object.mode_set(mode='EDIT')
-    arm_ob.hide = True
-    for ibone in range(24):
-        bb = arm_ob.data.edit_bones[obname + '_' + part_match['bone_%02d' % ibone]]
-        bboffset = bb.tail - bb.head
-        bb.head = joint_xyz[ibone]
-        bb.tail = bb.head + bboffset
-    bpy.ops.object.mode_set(mode='OBJECT')
-    return(shape)
-
-# load poses and shapes
-
+    return(ob, obname, arm_ob)
 
 def load_body_data(smpl_data, ob, obname, gender='female', idx=0):
     # load MoSHed data from CMU Mocap (only the given idx is loaded)
@@ -324,61 +185,13 @@ def main():
     log_message(argv)
 
     parser = argparse.ArgumentParser(description='Generate synth SMPL images.')
-    parser.add_argument('--idx', type=int, default=0,
-                        help='idx of the requested sequence')
-    parser.add_argument('--ishape', type=int, default=0,
-                        help='requested cut, according to the stride')
-    parser.add_argument('--stride', type=int, default=50,
-                        help='stride amount, default 50')
+    parser.add_argument('-s', '--split', choices=['train', 'test', 'all'], default='train', help='Split to use')
     args = parser.parse_args(argv)
-
-    idx = args.idx
-    ishape = args.ishape
-    stride = args.stride
-
-    log_message("input idx: %d" % idx)
-    log_message("input ishape: %d" % ishape)
-    log_message("input stride: %d" % stride)
-
-    if idx == None:
-        exit(1)
-    if ishape == None:
-        exit(1)
-    if stride == None:
-        log_message("WARNING: stride not specified, using default value 50")
-        stride = 50
-
-    # import idx info (name, split)
-    idx_info = load(open("/home/abhijit/Scratchspace/SURREAL/smpl_data/idx_info.pickle", 'rb'))
-
-    # get runpass
-    (runpass, idx) = divmod(idx, len(idx_info))
-
-    log_message("runpass: %d" % runpass)
-    log_message("output idx: %d" % idx)
-    idx_info = idx_info[idx]
-    log_message("sequence: %s" % idx_info['name'])
-    log_message("nb_frames: %f" % idx_info['nb_frames'])
-    log_message("use_split: %s" % idx_info['use_split'])
 
     smpl_data_folder = '/home/abhijit/Scratchspace/SURREAL/smpl_data'
     smpl_data_filename = 'smpl_data.npz'
     clothing_option = 'all'  # grey, nongrey or all
-
-    stepsize = 4    # subsampling MoCap sequence by selecting every 4th frame
-    stride = 50   # percent overlap between clips
-    clipsize = 100  # nFrames in each clip, where the random parameters are fixed
-
-    # compute number of cuts
-    nb_ishape = max(1, int(np.ceil((idx_info['nb_frames'] - (clipsize - stride)) / stride)))
-    log_message("Max ishape: %d" % (nb_ishape - 1))
-
-    if ishape == None:
-        exit(1)
-
-    assert(ishape < nb_ishape)
-
-    # >> don't use random generator before this point <<
+    split = 'train'
 
     genders = {0: 'female', 1: 'male'}
     # pick random gender
@@ -386,7 +199,7 @@ def main():
     log_message("Gender = %s" % gender)
 
     # grab clothing names
-    with open(join(smpl_data_folder, 'textures', '%s_%s.txt' % (gender, idx_info['use_split']))) as f:
+    with open(join(smpl_data_folder, 'textures', '%s_%s.txt' % (gender, args.split))) as f:
         txt_paths = f.read().splitlines()
     log_message("Found %d textures" % len(txt_paths))
 
@@ -430,13 +243,9 @@ def main():
 
     log_message("Initializing scene")
 
-    camera_params = {}
-    camera_params['camera_distance'] = np.random.normal(8.0, 1)
-    camera_params['resy'] = 320  # width
-    camera_params['resx'] = 240  # height
-
     fbx_file = join(smpl_data_folder, 'basicModel_%s_lbs_10_207_0_v1.0.2.fbx' % gender[0])
-    ob, obname, arm_ob, cam_ob = init_scene(scene, camera_params, fbx_file)
+    ob, obname, arm_ob = init_scene(scene, fbx_file)
+    cam_ob = bpy.data.objects['Camera']
 
     setState0()
     ob.select = True
@@ -446,7 +255,7 @@ def main():
     log_message("Creating materials segmentation")
     # create material segmentation
     if segmented_materials:
-        materials = create_segmentation(ob)
+        materials = create_segmentation(ob, join(smpl_data_folder, 'segm_per_v_overlap.pkl'))
         prob_dressed = {'leftLeg': .5, 'leftArm': .9, 'leftHandIndex1': .01,
                         'rightShoulder': .8, 'rightHand': .01, 'neck': .01,
                         'rightToeBase': .9, 'leftShoulder': .8, 'leftToeBase': .9,
@@ -459,27 +268,25 @@ def main():
         materials = {'FullBody': bpy.data.materials['Material']}
         prob_dressed = {'FullBody': .6}
 
-    orig_pelvis_loc = (arm_ob.matrix_world.copy() * arm_ob.pose.bones[obname + '_Pelvis'].head.copy()) - Vector((-1., 1., 1.))
-    orig_cam_loc = cam_ob.location.copy()
-
     # unblocking both the pose and the blendshape limits
     for k in ob.data.shape_keys.key_blocks.keys():
         bpy.data.shape_keys["Key"].key_blocks[k].slider_min = -10
         bpy.data.shape_keys["Key"].key_blocks[k].slider_max = 10
 
     log_message("Loading body data")
-    cmu_parms, fshapes, name = load_body_data(smpl_data, ob, obname, idx=idx, gender=gender)
+    cmu_parms, fshapes, name = load_body_data(smpl_data, ob, obname, idx=0, gender=gender)
+
 
     log_message("Loaded body data for %s" % name)
 
     nb_fshapes = len(fshapes)
-    if idx_info['use_split'] == 'train':
+    if args.split == 'train':
         fshapes = fshapes[:int(nb_fshapes * 0.8)]
-    elif idx_info['use_split'] == 'test':
+    elif args.split == 'test':
         fshapes = fshapes[int(nb_fshapes * 0.8):]
 
     # pick random real body shape
-    shape = choice(fshapes)  # +random_shape(.5) can add noise
+    # shape = choice(fshapes)  # +random_shape(.5) can add noise
     #shape = random_shape(3.) # random body shape
 
     # example shapes
@@ -500,12 +307,8 @@ def main():
         scs[-1].filepath = sh_dst
         scs[-1].update()
 
-    data = cmu_parms[name]
-
     arm_ob.animation_data_clear()
     cam_ob.animation_data_clear()
-
-    # scene.node_tree.nodes['Image'].image = bg_img
 
     for _, material in materials.items():
         material.node_tree.nodes['Vector Math'].inputs[1].default_value[:2] = (0, 0)
