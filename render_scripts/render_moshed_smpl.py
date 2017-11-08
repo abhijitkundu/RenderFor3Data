7f4fa547e669dde062c39ba166e0192f2acf5555
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-11-08 01:13:13 -0500

    now using SMPLBody class

--- render_scripts/render_moshed_smpl.py
+++ render_scripts/render_moshed_smpl.py
@@ -5,7 +5,7 @@ import numpy as np
 from random import choice
 import bpy
 from mathutils import Matrix, Vector, Quaternion, Euler
-from RenderFor3Data.smpl_helper import create_body_segmentation, create_shader_material
+from RenderFor3Data.smpl_helper import SMPLBody, create_body_segmentation, create_shader_material
 from RenderFor3Data.blender_helper import (deselect_all_objects,
                                            get_camera_intrinsic_from_blender,
                                            set_blender_camera_extrinsic,
@@ -40,18 +40,24 @@ def setup_scene(scene):
     scene.render.layers['RenderLayer'].use_pass_emit = True
     scene.render.layers['RenderLayer'].use_pass_material_index = True
 
-    # OSL is supported only in CPU
-    scene.cycles.device = 'GPU'
-    cycles_prefs = bpy.context.user_preferences.addons['cycles'].preferences
-    cycles_prefs.compute_device_type = "CUDA"
+    # OSL is NOT supported only in CPU
+    # scene.cycles.device = 'GPU'
+    # cycles_prefs = bpy.context.user_preferences.addons['cycles'].preferences
+    # cycles_prefs.compute_device_type = "CUDA"
 
 
 def set_blender_camera(scene, cam):
     """Setup camera intrinsics"""
-    W = 1600
-    H = 800
-    K = Matrix(((1750.0, 0.0, 800.0),
-                (0.0, 1750.0, 400.0),
+    # W = 1600
+    # H = 800
+    # K = Matrix(((1750.0, 0.0, 800.0),
+    #             (0.0, 1750.0, 400.0),
+    #             (0.0,    0.0,   1.0)))
+
+    W = 800
+    H = 400
+    K = Matrix(((875.0, 0.0, 400.0),
+                (0.0, 875.0, 200.0),
                 (0.0,    0.0,   1.0)))
     # set render size
     scene.render.resolution_x = W
@@ -63,36 +69,6 @@ def set_blender_camera(scene, cam):
 
     # Set camera extrinsic to Identity
     set_blender_camera_extrinsic(cam, Matrix.Identity(3), Vector.Fill(3, 0.0))
-
-
-def add_person_to_scene(smpl_data_dir, gender, obj_idx):
-    """
-    Add a SMPL person to scene
-    """
-    if gender == 'male':
-        fbx_file = osp.join(smpl_data_dir, 'basicModel_m_lbs_10_207_0_v1.0.2.fbx')
-    elif gender == 'female':
-        fbx_file = osp.join(smpl_data_dir, 'basicModel_f_lbs_10_207_0_v1.0.2.fbx')
-    else:
-        raise NotImplementedError
-
-    assert osp.exists(fbx_file), "{} does not exist".format(fbx_file)
-
-    bpy.ops.import_scene.fbx(filepath=fbx_file, axis_forward='Y', axis_up='Z', global_scale=100)
-
-    arm_ob = bpy.context.active_object
-    mesh_ob = arm_ob.children[0]
-    assert '_avg' in mesh_ob.name
-
-    arm_ob.name = 'Person{:02d}'.format(obj_idx)
-    mesh_ob.name = 'Person{:02d}_mesh'.format(obj_idx)
-    mesh_ob.data.use_auto_smooth = False  # autosmooth creates artifacts
-
-    # clear existing animation data
-    mesh_ob.data.shape_keys.animation_data_clear()
-    arm_ob.animation_data_clear()
-
-    return(mesh_ob, arm_ob)
 
 def main():
     """Main Function"""
@@ -160,45 +136,24 @@ def main():
     assert osp.exists(cloth_img_path), "{} does not exist".format(cloth_img_path)
     create_shader_material(material.node_tree, sh_script, cloth_img_path)
 
-    mesh_ob, arm_ob = add_person_to_scene(smpl_data_dir, 'male', obj_id)
-    # assign the existing spherical harmonics material
-    # mesh_ob.active_material = bpy.data.materials['Material']
-
-    deselect_all_objects()
-    mesh_ob.select = True
-    scene.objects.active = mesh_ob
-    segmented_materials = True  # True: 0-24, False: expected to have 0-1 bg/fg
-
-    # create material segmentation
-    if segmented_materials:
-        materials = create_body_segmentation(vsegm, mesh_ob, material)
-        prob_dressed = {'leftLeg': .5, 'leftArm': .9, 'leftHandIndex1': .01,
-                        'rightShoulder': .8, 'rightHand': .01, 'neck': .01,
-                        'rightToeBase': .9, 'leftShoulder': .8, 'leftToeBase': .9,
-                        'rightForeArm': .5, 'leftHand': .01, 'spine': .9,
-                        'leftFoot': .9, 'leftUpLeg': .9, 'rightUpLeg': .9,
-                        'rightFoot': .9, 'head': .01, 'leftForeArm': .5,
-                        'rightArm': .5, 'spine1': .9, 'hips': .9,
-                        'rightHandIndex1': .01, 'spine2': .9, 'rightLeg': .5}
+    if gender == 'male':
+        fbx_file = osp.join(smpl_data_dir, 'basicModel_m_lbs_10_207_0_v1.0.2.fbx')
     else:
-        materials = {'FullBody': material}
-        prob_dressed = {'FullBody': .6}
+        fbx_file = osp.join(smpl_data_dir, 'basicModel_f_lbs_10_207_0_v1.0.2.fbx')
 
-    # unblocking both the pose and the blendshape limits
-    for k in mesh_ob.data.shape_keys.key_blocks.keys():
-        bpy.data.shape_keys["Key"].key_blocks[k].slider_min = -10
-        bpy.data.shape_keys["Key"].key_blocks[k].slider_max = 10
+    smpl_body = SMPLBody(fbx_file, material, obj_id, vertex_segm=vsegm)
 
-    mesh_ob.active_material = material
+    # TODO Check if this is required
+    # mesh_ob.active_material = material
 
     # spherical harmonics material needs a script to be loaded and compiled
     spherical_harmonics = []
-    for _, material in materials.items():
+    for _, material in smpl_body.materials.items():
         spherical_harmonics.append(material.node_tree.nodes['Script'])
         spherical_harmonics[-1].filepath = sh_script
         spherical_harmonics[-1].update()
 
-    for _, material in materials.items():
+    for _, material in smpl_body.materials.items():
         material.node_tree.nodes['Vector Math'].inputs[1].default_value[:2] = (0, 0)
 
     # set up random light
