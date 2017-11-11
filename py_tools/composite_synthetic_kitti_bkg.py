20196b2d8f28a105a107c99a191f8b43ca4ff3a0
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-11-11 14:59:10 -0500

    script now saves the composite images

--- py_tools/composite_synthetic_kitti_bkg.py
+++ py_tools/composite_synthetic_kitti_bkg.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 
 """
-This script reads an image dataset file and saves each image info in a separate file.
+This script creates composite image from FG and BG image
 """
 import argparse
 import json
 import os.path as osp
+from os import makedirs
 from glob import glob
 import numpy as np
 import cv2
@@ -19,13 +20,18 @@ def main():
     """Main Function"""
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", "--fg_image_dir", required=True, type=str, help="Path to FG image directory")
-    parser.add_argument("-b", "--bg_image_dir", required=True, type=str, help="Path to BG image  directory")
+    parser.add_argument("-b", "--bg_image_dir", required=True, type=str, help="Path to BG image directory")
+    parser.add_argument("-o", "--out_image_dir", type=str, help="Path to output directory for Composite image ")
     args = parser.parse_args()
 
-    assert osp.isdir(args.fg_image_dir), '"{}" is not a directory'.format(args.fg_image_dir)
-    assert osp.exists(args.fg_image_dir), '"{}" do not exist'.format(args.fg_image_dir)
-    assert osp.isdir(args.bg_image_dir), '"{}" is not a directory'.format(args.bg_image_dir)
-    assert osp.exists(args.bg_image_dir), '"{}" do not exist'.format(args.bg_image_dir)
+    assert osp.isdir(args.fg_image_dir), '"{}" does not exist or not a directory'.format(args.fg_image_dir)
+    assert osp.isdir(args.bg_image_dir), '"{}" does not exist or not a directory'.format(args.bg_image_dir)
+
+    pause_time = 0
+    if args.out_image_dir:
+        if not osp.isdir(args.out_image_dir):
+            makedirs(args.out_image_dir)
+        pause_time = 1
 
     fg_image_paths = glob(osp.join(args.fg_image_dir, '*.png'))
     assert fg_image_paths, 'No FG images found'
@@ -35,7 +41,8 @@ def main():
         assert fg_image.ndim == 3, "Image needs to be a 3D tensor"
         assert fg_image.shape[2] == 4, "FG Image needs to be a 4 channel (with alpha channel)"
 
-        bg_image_path = osp.join(args.bg_image_dir, osp.basename(fg_image_path)[:8] + '_color.png')
+        image_name = osp.basename(fg_image_path)[:6] + '.png'
+        bg_image_path = osp.join(args.bg_image_dir, image_name)
         assert osp.exists(bg_image_path), 'No BG image found at {}'.format(bg_image_path)
 
         bg_image = cv2.imread(bg_image_path, cv2.IMREAD_COLOR)
@@ -46,11 +53,17 @@ def main():
         fg_image = fg_image[:, :, :3].astype(np.float) / 255
         bg_image = bg_image.astype(np.float) / 255
 
-        composite_image = alpha * fg_image + (1. - alpha) * bg_image
+        composite_image = (alpha * fg_image + (1. - alpha) * bg_image) * 255
+        composite_image = composite_image.astype(np.uint8)
+
+        if args.out_image_dir:
+            out_image_path = osp.join(args.out_image_dir, image_name)
+            cv2.imwrite(out_image_path, composite_image)
+            print("Saved composite_image at {}".format(out_image_path))
 
         cv2.namedWindow('composite_image', cv2.WINDOW_NORMAL)
         cv2.imshow('composite_image', composite_image)
-        key = cv2.waitKey(0)
+        key = cv2.waitKey(pause_time)
         if key == 27:
             cv2.destroyAllWindows()
             break
