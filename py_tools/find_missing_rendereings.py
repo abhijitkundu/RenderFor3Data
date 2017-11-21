4b08f2a324c6373a8b6552d02a27a2bf98d51d49
Abhijit Kundu <abhijit.kundu@gatech.edu>
2017-11-21 17:58:40 -0500

    no hardcoded paths

--- py_tools/find_missing_rendereings.py
+++ py_tools/find_missing_rendereings.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 
 import os.path as osp
+import argparse
 
 def main():
     """Main Function"""
-    for i in range(20000):
-        image_file = "../data/FlyingCars20k_seed42/color_cyc/{:08d}_color.png".format(i)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("image_dir", type=str, help="Path to image dir")
+    parser.add_argument("-n", "--num_of_images", default=20000, type=int, help="Number of expected images")
+    args = parser.parse_args()
+
+    assert osp.isdir(args.image_dir), "{} does not exist or not a directory".format(args.image_dir)
+
+    for i in range(args.num_of_images):
+        image_file = osp.join(args.image_dir, "{:08d}_color.png".format(i))
         if not osp.exists(image_file):
-            info_file = "../data/FlyingCars20k_seed42/image_infos/{:08d}_info.json".format(i)
+            info_file = "{:08d}_info.json".format(i)
             print(info_file)
 
 if __name__ == '__main__':
